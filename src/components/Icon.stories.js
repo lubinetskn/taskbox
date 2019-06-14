65d41293cb58b47d1a54de3749d7564a36d065f2
Лубинец Константин Николаевич <konstantin.lubinets@ligastavok.ru>
2019-06-14 13:56:28 +0300

    toomaniu

--- src/components/Icon.stories.js
+++ src/components/Icon.stories.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { storiesOf } from '@storybook/react';
 
-import { TooltipLinkList } from '@storybook/components';
-import { Icons } from '@storybook/components';
+import IconList from './icon/index'
+import { Logo, Android, ArrowDown, ArrowLeft, ArrowRight, ArrowUp, ArrowFull, ArrowMenu, Card, ChangeBase, ChangeOdds, Circle, Close, Coupon, DangerWarning, DefSumm, Delay, Delete, Deposit, Desktop, Done, Face, Gift, Heart, Home, Info, Ios, Lock, Menu, Mobile, MyAccount, MyStakes, Ok, Pin, Quick, Reload, Search, Settings, StarFilled, StarOutline, Text, Video, Statement, VisibleOff, VisibleOn, Warning, Widget, Withdraw, Chat, Email,Telegram ,Viber , Return} from 'lib-ui__icons';
 
-export default {
-  component: Icons,
-  title: 'UI|Sidebar/ListItemIcon',
-};
+console.log(IconList);
 
-export const all = () => (
-  <TooltipLinkList
-    links={[
-      { title: 'has icon', left: <Icons icon="check" /> }
-    ]}
-  />
-);
+storiesOf('Icons', module)
+  .add('Icons List SVG', () => {
+    let arr =  Object.entries(IconList);
+    return arr.map((elem) => {
+      return <img src={elem[1]} alt={elem[0]} />
+    })
+  })
+  .add('Icons List LibUi', () => {
+    let arr =  [Logo, Android, ArrowDown, ArrowLeft, ArrowRight, ArrowUp, ArrowFull, ArrowMenu, Card, ChangeBase, ChangeOdds, Circle, Close, Coupon, DangerWarning, DefSumm, Delay, Delete, Deposit, Desktop, Done, Face, Gift, Heart, Home, Info, Ios, Lock, Menu, Mobile, MyAccount, MyStakes, Ok, Pin, Quick, Reload, Search, Settings, StarFilled, StarOutline, Text, Video, Statement, VisibleOff, VisibleOn, Warning, Widget, Withdraw, Chat, Email,Telegram ,Viber , Return];
+    return arr.map((elem) => {
+      return elem
+    })
+  });
